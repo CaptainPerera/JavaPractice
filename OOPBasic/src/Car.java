5da3f895320ab271cfaab8ff343fc4080521a25a
Umeen Rathnayake <umeenbg16@gmail.com>
2024-01-07 23:52:12 +0530

    New Practices

---
+++ OOPBasic/src/Car.java
@@ -0,0 +1,14 @@
+public class Car {
+    String make = "Toyota";
+    String color= "Blue";
+    int year = 1999;
+    double price = 1000000.00;
+
+    void drive(){
+        System.out.println("You drive the car.");
+    }
+
+    void stop(){
+        System.out.println("You stopped the car.");
+    }
+}
---
+++ OOPBasic/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Human hu1 = new Human("Umeen", 25, 71.0);
+        Human hu2 = new Human("Jayani", 25, 45.0);
+
+        hu1.eat();
+        hu2.drink();
+    }
+}
